ee3a4856a883a94e984d4e91fa6b66fd7f4df157
rodrigo453 <rodrigochicora.azevedo@gmail.com>
2022-09-25 14:35:14 -0300

    Criada as funções de listagem e cadastro

--- Controllers/ContasController.cs
+++ Controllers/ContasController.cs
@@ -1,4 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using PagueSempre.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace PagueSempre.Controllers
 {
-    
+    [ApiController]
+    [Route("api/Contas")]
+
+    public class ContasController : ControllerBase
+    {
+    private readonly DataContext _context;
+    public ContasController(DataContext context) => _context = context;
+    private static List<Contas> contas = new List<Contas>();
+
+    //Get /api/estoque/listar
+    [Route("listar")]
+    [HttpGet]
+    public IActionResult Listar() =>
+        Ok(_context.Contas.ToList());
+
+    //POST /api/estoque/cadastrar
+    [Route("cadastrar")]
+    [HttpPost]
+    public IActionResult Cadastrar([FromBody] Contas contas)
+    {
+        _context.Contas.Add(contas);
+        _context.SaveChanges();
+        return Created("", contas);
+    }     
+
+
+
+    }
 }
